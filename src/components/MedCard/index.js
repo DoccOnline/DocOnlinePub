574037366116ef0a6455b19a925a7d5eac9c3897
Zoriana <zoryana.lesyk@experteer.com>
2020-04-26 00:51:43 +0200

    Med card

--- src/components/MedCard/index.js
+++ src/components/MedCard/index.js
@@ -1,10 +1,60 @@
 import React from 'react';
+import {Button, IconButton} from "@material-ui/core";
+import AlarmIcon from "@material-ui/core/SvgIcon/SvgIcon";
+import {Link} from "react-router-dom";
+import "./MedCard.css"
 
 const MedCard = ({}) => {
 
 	return (
-		<div>
-			MedCard
+		<div className="AppointmentInfo">
+			<div className="table">
+				<div className="row">
+					<div className="col left">Щеплення</div>
+					<div className="col right">
+						<Link to='#'>
+							Натисни щоб переглянути календар щеплень
+						</Link>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col left">Група крові</div>
+					<div className="col right">
+						А+
+					</div>
+				</div>
+				<div className="row">
+					<div className="col left">ЕКГ</div>
+					<span className="col right">
+					<Link to='#'>
+							Натисни щоб переглянути всі ресурси
+						</Link>
+					</span>
+				</div>
+				<div className="row">
+					<div className="col left">Pентген знімки</div>
+					<span className="col right">
+					<Link to='#'>
+							Натисни щоб переглянути всі ресурси
+						</Link>
+					</span>	</div>
+				<div className="row">
+					<div className="col left">Аналізи</div>
+					<span className="col right">
+						<Link to='#'>
+							Натисни щоб переглянути всі ресурси
+						</Link>
+					</span>
+				</div>
+				<div className="row">
+					<div className="col left">Заключення лікаря</div>
+					<span className="col right">
+							<Link to='#'>
+							Натисни щоб переглянути всі ресурси
+						</Link>
+					</span>
+				</div>
+			</div>
 		</div>
 	)
 }
