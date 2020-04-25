5e94eef43dc4898facf0edf16ad718cc9af52e79
Zoriana <zoryana.lesyk@experteer.com>
2020-04-26 00:39:32 +0200

    Add recommendations

--- src/components/Recommendations/index.js
+++ src/components/Recommendations/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import {Button, IconButton} from "@material-ui/core";
+import AlarmIcon from "@material-ui/core/SvgIcon/SvgIcon";
+import recico from '../../icons/Vectorreccom.svg'
+
+import "./Recommendations.css"
 
 const Recommendations = ({}) => {
 
 	return (
-		<div>
-			Recommendations
+		<div className="Recommendations">
+			{
+				[1,2,3].map(el => {
+					return (
+						<div className="rec-card">
+							<div className="rec-wrap">
+								<div className="inside">
+									<img src={recico} alt=""/>
+								</div>
+							</div>
+							<div className="rec-content">
+								<p className="fm-name">Рекомендації</p>
+								<p className='fm-part'>16 травня - 14:00</p>
+							</div>
+						</div>
+					)
+				})
+			}
 		</div>
 	)
-}
+};
 
 export default Recommendations
